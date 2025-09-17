5cfd6c7fe6d4838e81e07b86b8606b3e118c945b
srijan7044 <offsray7044@gmail.com>
2025-09-17 15:41:18 +0530

    Describe what you changed

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      'https://simple-expense-tracker-qvgo.onrender.com',
+      'https://simple-expense-tracker-smoky.vercel.app/',
       'http://localhost:5173'
     ],
     credentials: true
